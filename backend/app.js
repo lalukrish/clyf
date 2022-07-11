25253cd4ccc6bc9fd3f06b4421d562c7627330e1
Lal krishna N C <lalukrishnavallil01@gmail.com>
2022-07-11 14:35:49 +0530

    backend started

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import router from "./routes/user";
+app.use(express.json());
+app.use("/api/user", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://user_user-_123:user_user-_123@cluster0.aqzbj.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log("connected to the database and port5000"))
+  .catch((err) => console.log(err));
---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+import User from "../model/User";
+
+export const getAllUser = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!users) {
+    return res.status(404).json({ message: "No users found" });
+  }
+  return res.status(200).json({ users });
+};
+
+export const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+  }
+  if (existingUser) {
+    return res.status(400).json({ message: "user already is exist" });
+  }
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+  try {
+    user.save();
+  } catch (err) {
+    console.log(err);
+  }
+  return res.status(201).json({ user });
+};
