b60b3e2957f0e5c0d6304653316497a5aed27b5e
Lal krishna N C <lalukrishnavallil01@gmail.com>
2022-08-03 16:20:55 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ne eathadi naaye</h1>
+        <h1>ne eathaada naaye!!</h1>
       </header>
     </div>
   );
