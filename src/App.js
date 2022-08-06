e6fd97ef5edeb6187a0ce3930c11061cb97dd360
Lal krishna N C <lalukrishnavallil01@gmail.com>
2022-08-06 13:57:38 +0530

    designing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import "./App.css";
-
+import Header from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import AddBlog from "./components/AddBlog";
+import BlogDetails from "./components/BlogDetails";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ne eathaada naaye!!</h1>
+    <React.Fragment>
+      <header>
+        <Header />
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetails />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { AppBar, Button, Toolbar, Typography, Box, Tabs } from "@mui/material";
+const Header = () => {
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 46%, rgba(0,212,255,1) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h4">Blogapp</Typography>
+        <Box display="flex">
+          <Tabs>
+            <Tab label="All Blogs" />
+          </Tabs>
+        </Box>
+        <Box display="flex" marginLeft="auto">
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            LOGIN
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Signup
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
